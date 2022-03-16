368d68012994dd248f6f7bbda5dc700ad62571bf
DESKTOP-5ABV6RE\hafifa-14 <alon4688@gmail.com>
2022-03-16 17:36:16 +0200

    Starting

--- final-react-project/src/components/App.tsx
+++ final-react-project/src/components/App.tsx
@@ -4,9 +4,13 @@ import PeoplePage from './PeoplePage';
 import GroupsPage from './GroupsPage';
 import HomePage from './homePage';
 import FourOFourPage from './404page';
+import { Button } from '@mui/material';
+import Cookies from 'js-cookie';
 
-const App: FC = () => (
-    <div className='App'>
+const App: FC = () => {
+    return <div className='App'>
+        {() => Cookies.set('xd', 'your mom')}
+        <Button onClick={() => console.log(Cookies.get('xd'))}><p>xd</p></Button>
         <Routes>
             <Route path='/' element={<HomePage />}/>
             <Route path='/peoplepage' element={<PeoplePage />}/>
@@ -15,6 +19,6 @@ const App: FC = () => (
             <Route path='*' element ={<Navigate replace={true} to='/404page'/>}/>
         </Routes>
     </div>
-);
+};
 
 export default App;
